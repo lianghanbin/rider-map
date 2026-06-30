2c8340b46333b6f83a55dcd8b44624ee13aa180d
梁汉斌 <lianghanbin47@outlook.com>
2026-06-30 22:08:08 +0800

    搜索

--- config.example.js
+++ config.example.js
@@ -3,4 +3,4 @@
 //   1. 把这个文件复制一份，改名为 config.js
 //   2. 把下面的 YOUR_KEY_HERE 替换成你的真实 Key
 //   3. config.js 在 .gitignore 里，不会被上传到 git
-var AMAP_KEY = 'YOUR_KEY_HERE';
+var AMAP_KEY = '11e264e6efceed06fd31996c56b15771';

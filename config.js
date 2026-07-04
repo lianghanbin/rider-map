849acc8967a7aa857b0206bb026dfd3f8b820fa5
梁汉斌 <lianghanbin47@outlook.com>
2026-07-04 17:22:03 +0800

    3.1

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
 // 高德地图 API Key
 var AMAP_KEY = '11e264e6efceed06fd31996c56b15771';
-// Supabase 配置
-var SUPABASE_URL = 'https://cbipjaliwgxaedcwfmey.supabase.co';
-var SUPABASE_KEY = 'sb_publishable_k8AjVcewmAqRVwyHGMlBlQ_2c4_3wMu';
